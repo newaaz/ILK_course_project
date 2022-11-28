1b00e7a69e3b83e7bde64100fc267ce64f75e2ea
jumper <jumperka@list.ru>
2022-11-28 20:07:36 +0300

    listing hotels by category

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -7,5 +7,14 @@ class TownsController < ApplicationController
   # all town hotels
   def hotels
     @town = Town.find(params[:id])
+    @categories = Category.all
+
+    if params[:cat].blank?
+      @properties = @town.properties
+    else
+      @properties = @town.properties.where(category_id: params[:cat])
+      @properties_category = Category.find(params[:cat]).title
+    end    
   end 
 end
+
