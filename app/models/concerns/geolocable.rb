d57b8244ef1da2a2fc9333badadd1d40732b017c
jumper <jumperka@list.ru>
2023-01-24 11:36:45 +0300

    refactor find nearbys objects

--- app/models/concerns/geolocable.rb
+++ app/models/concerns/geolocable.rb
@@ -2,30 +2,28 @@ module Geolocable
   extend ActiveSupport::Concern
   included do
     has_one :geolocation, as: :geolocable, dependent: :destroy
-    accepts_nested_attributes_for :geolocation, reject_if: :coordinates_optional?
+    accepts_nested_attributes_for :geolocation
 
     def nearby_objects(object)
       min_lat, max_lat = (geolocation.latitude - 0.0090), (geolocation.latitude + 0.0090)
       min_long, max_long = (geolocation.longitude - 0.0127), (geolocation.longitude + 0.0127)
 
-      points = Geolocation.where(latitude: min_lat..max_lat, longitude: min_long..max_long, geolocable_type: object)
-                          .where.not(geolocable_type: object, geolocable_id: id)
+      nearbys_geolocations = Geolocation.where(latitude: min_lat..max_lat, longitude: min_long..max_long, geolocable_type: object)
+                                 .where.not(geolocable_type: object, geolocable_id: id)
+                                 .order(:updated_at)
+      
+      nearbys_objects = object.constantize.order(:updated_at).find(nearbys_geolocations.pluck(:geolocable_id))
 
-      points.map do |point|
+      nearbys_objects.map.with_index do |object, i|
         {
-          title: point.geolocable.title,
-          object_id: point.geolocable.id,
-          distance: distance_to(point)
+          title: object.title,
+          object_id: object.id,
+          distance: distance_to(nearbys_geolocations[i])
         }
-      end 
+      end
     end
 
     private
-
-    def coordinates_optional?
-      #TODO set name for array optional_models
-      %w[Activity].include?(model_name.name)
-    end
 
     def distance_to(point)
       (120000 * Math.sqrt((geolocation.latitude - point.latitude)**2 + (geolocation.longitude - point.longitude)**2)).floor(-1)
