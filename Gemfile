8d1cea2f177548231681d06af26675c2c23da886
jumper <jumperka@list.ru>
2023-08-15 10:58:46 +0300

    added seotags (minimum)

--- Gemfile
+++ Gemfile
@@ -65,6 +65,8 @@ gem "bullet", "~> 7.0"
 
 gem "pagy", "~> 6.0"
 
+gem "meta-tags", "~> 2.18"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  include ActionView::Helpers::AssetUrlHelper
+  
   def home
     @towns = Town.all
     @properties = Property.activated.where(promouted: 5).select(:id, :title, :avatar, :images, :town_id, :category_id, :services, :address, :price_from, :distance_to_sea).take 6
@@ -6,6 +8,18 @@ class StaticPagesController < ApplicationController
     @services = Service.activated.where(promouted: 5).select(:id, :title, :avatar, :images, :category_title, :price, :price_type, :address).take 6
     @places = Place.activated.where(promouted: 5).select(:id, :title, :avatar, :images, :category_title, :town_id, :address).take 6
     @food_places = FoodPlace.where(promouted: 5).activated.select(:id, :title, :avatar, :images, :tags, :town_id, :address).take 6
+
+    set_meta_tags(
+      description: "жильё, активный отдых, услуги и сервис в Крыму на сайте Люблю Крым - ilovekrim.ru",
+      og: {
+        title: "Люблю Крым - ilovekrim.ru",
+        description: "жильё, активный отдых, услуги и сервис в Крыму на сайте Люблю Крым - ilovekrim.ru",
+        type: "website",
+        url: url_for(:only_path => false),
+        image: image_url("theme/category/12.jpg"),
+        locale: "ru_RU"
+      }
+    )
   end
 
   def contacts
